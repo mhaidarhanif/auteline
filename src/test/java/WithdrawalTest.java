d5fcf295a7cec49d5afdeaaf2f3d0c12015749e8
cbitt <csbango@yahoo.com>
2018-04-06 02:50:57 +0100

    added test to fail

---
+++ src/test/java/WithdrawalTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+public class WithdrawalTest {
+    @org.junit.Test
+    public void dummyPass() throws Exception {
+        int a = 1;
+        int b = 2;
+        assertTrue(a + b == 3);
+    }
+
+    @org.junit.Test
+    public void dummyFail() throws Exception {
+        int a = 1;
+        int b = 2;
+        assertTrue(a + b == 33);
+    }
+
+}
