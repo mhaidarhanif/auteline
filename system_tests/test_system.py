a46ac58a2076db27e8c38a97d1f3033446a9bc3e
Emil Harvey <36806437+ehharvey@users.noreply.github.com>
2022-10-18 14:17:02 -0400

    Update test_system.py
    Fixed some old comments.

--- system_tests/test_system.py
+++ system_tests/test_system.py
@@ -1,10 +1,7 @@
 import pexpect 
 
 def test_systemTest():
-    #Need to create jar file for this to run, 
-    #having trouble creating one without META-INF/MANIFEST.MF
-    #spent a long time trying to make jar from command line 
-    #with commands like jar cfm test.jar manifest.mf ATMTest.class
+    # Spawn the test via gradle.
     child = pexpect.spawn('gradle run')
 
     child.expect_exact('[i] Welcome to Auteline Bank ATM!')
